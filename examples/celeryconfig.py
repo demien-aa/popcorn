b97dcde2a6b258d5688705abc1080d54e9e61b02
StevenYang-aa <steven@appannie.com>
2016-06-18 13:43:19 +0800

    support celeryconfig local

--- examples/celeryconfig.py
+++ examples/celeryconfig.py
@@ -1,3 +1,5 @@
+import os
+
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
@@ -5,5 +7,11 @@ CELERYD_POOL_RESTARTS = True
 CELERY_IMPORTS = (
     "tasks",
 )
-HUB_IP = '172.17.0.2'
-BROKER_URL = 'redis://localhost:6379/0'
+
+HUB_IP = '192.168.1.52'
+BROKER_URL = 'redis://%s:6379/0' % HUB_IP
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+if os.path.exists(os.path.join(ROOT, "celeryconfig_local.py")):
+    execfile(os.path.join(ROOT, "celeryconfig_local.py"))
